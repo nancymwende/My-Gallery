1061adf98b9ba4e243544c2becdb659b66ace84e
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-28 01:06:59 +0300

    ft:make migrations for pictures

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class   Images(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    # location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    
