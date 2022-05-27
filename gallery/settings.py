287a636c41d8d4f66c2b984877c0d0ced3b5dfb6
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-27 16:02:54 +0300

    style:(i) add bootstrap folder. (ii)add text colour to blue & position heading to center

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pictures.apps.PicturesConfig'
+    'pictures.apps.PicturesConfig',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
