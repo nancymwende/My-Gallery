61e00b9d6b9171c5f0fa70b12942ddf1c1775414
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-28 11:20:16 +0300

    ft:create test for location & category models

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,28 @@
+from unicodedata import name
 from django.test import TestCase
+from .models import Location, Category
 
 # Create your tests here.
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        Location.objects.create(name="Image Location")
+        
+    def test_location_name(self):
+
+        location = Location.objects.get(name="Image Location")
+        self.assertEqual(location.name, "Image Location")    
+        
+        
+        
+        
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name="Image Category")
+        
+    def test_category_name(self):
+
+        category = Category.objects.get(name="Image Category")
+        self.assertEqual(category.name, "Image Category")
+        
+
