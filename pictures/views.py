740277fcf3ca3ca7024c99430e7b429ab5c10464
bkee9 <nancy.mwende@student.moringaschool.com>
2022-05-31 19:17:21 +0300

    push to heroku master

--- pictures/views.py
+++ pictures/views.py
@@ -3,8 +3,6 @@ from django.http  import HttpResponse
 from .models import Category,Location,Image
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'index.html')
     
 def images(request):
     images = Image.objects.all()
